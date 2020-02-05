62cd5f49b912ce7ca96c2fa64dcf8f47c03b8338
Matt Pennick <contact@mattpennick.com>
2020-02-05 18:57:43 +0000

    2PM Break Day 3

--- src/app/features/shopping/reducers/index.ts
+++ src/app/features/shopping/reducers/index.ts
@@ -1,8 +1,30 @@
 export const featureName = 'shoppingFeature';
-export interface ShoppingState {
+import * as fromList from './list.reducer';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { ShoppingItemModel } from '../models';
 
+export interface ShoppingState {
+  list: fromList.ListState;
 }
 
 export const reducers = {
-
+  list: fromList.reducer
 };
+
+
+// Selectors
+
+// 1. Create a feature
+const selectFeature = createFeatureSelector<ShoppingState>(featureName);
+// 2. 1 per branch
+const selectListBranch = createSelector(selectFeature, f => f.list);
+// 3. Helpers
+const { selectAll: selectAllListItems } = fromList.adapter.getSelectors(selectListBranch);
+
+
+// 4. what components need
+
+// TODO ShopingItemModel[]
+export const selectShoppingItemModel = createSelector(selectAllListItems,
+  (items) => items as ShoppingItemModel[]
+);
